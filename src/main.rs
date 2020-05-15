75d035698d5bdd579d70116b056694fd78682da3
rasmusmerzin <rasmusmerzin@gmail.com>
2020-05-15 14:59:21 +0300

    added link listing to list operator

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,23 @@ fn list(pkg_path: &Path, bin_path: &Path) {
         for exe in get_exes(repo.as_path()) {
             if let Some(filename_os) = exe.file_name() {
                 if let Some(filename) = filename_os.to_str() {
-                    println!(
-                        "  {} ({})",
-                        filename,
-                        get_links_to(exe.as_path(), bin_path).len()
-                    );
+                    let links = get_links_to(exe.as_path(), bin_path);
+                    if links.len() == 0 {
+                        println!("  {}", filename);
+                    } else {
+                        let mut link_list = String::new();
+                        for link in links {
+                            if let Some(filename_os) = link.file_name() {
+                                if let Some(filename) = filename_os.to_str() {
+                                    if !link_list.is_empty() {
+                                        link_list.push_str(", ");
+                                    }
+                                    link_list.push_str(filename);
+                                }
+                            }
+                        }
+                        println!("  {} <- {}", filename, link_list);
+                    }
                 }
             }
         }
