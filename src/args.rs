89b59abf21f7cb1e4494e950377fe2f122e1ff1f
rasmusmerzin <rasmusmerzin@gmail.com>
2020-05-16 21:40:13 +0300

    argument parsing init

---
+++ src/args.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+pub fn parse_args(
+    args: &[String],
+    valid_flags: &[&str],
+    valid_opts: &[&str],
+) -> Result<(Vec<String>, Vec<String>, HashMap<String, String>), String> {
+    let mut flags: Vec<String> = vec![];
+    let mut words: Vec<String> = vec![];
+    let mut opts: HashMap<String, String> = HashMap::new();
+    let mut listener_opt = String::new();
+
+    for a in args {
+        if !listener_opt.is_empty() {
+            opts.insert(listener_opt.clone(), String::from(a));
+            listener_opt.clear();
+        } else {
+            if a.starts_with("--") {
+                let f = &a[2..];
+                if valid_opts.contains(&f) {
+                    listener_opt = String::from(f);
+                } else if valid_flags.contains(&f) {
+                    flags.push(String::from(f));
+                } else {
+                    return Err(format!("unknown flag '--{}'", f));
+                }
+            } else if a.starts_with("-") {
+                for c in a.chars().skip(1) {
+                    let f = &c.to_string();
+                    if valid_opts.contains(&f.as_str()) {
+                        listener_opt = String::from(f);
+                    } else if valid_flags.contains(&f.as_str()) {
+                        flags.push(String::from(f));
+                    } else {
+                        return Err(format!("unknown flag '-{}'", f));
+                    }
+                }
+            } else {
+                words.push(String::from(a));
+            }
+        }
+    }
+
+    if !listener_opt.is_empty() {
+        return Err(format!("option '-{}' requires a value", listener_opt));
+    }
+
+    Ok((flags, words, opts))
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use output::{error, msg, print_usage};
 mod query;
 use query::{get_exes, get_links_to, get_repos};
 
+mod args;
+use args::parse_args;
+
 fn list(pkg_path: &Path, bin_path: &Path, query: &[String]) {
     for repo in get_repos(pkg_path, pkg_path, &query) {
         if let Ok(rel_path) = repo.strip_prefix(pkg_path) {
@@ -42,34 +45,47 @@ fn list(pkg_path: &Path, bin_path: &Path, query: &[String]) {
 }
 
 fn main() {
-    let args: Vec<String> = args().collect();
+    match parse_args(&args().collect::<Vec<String>>()[1..], &["c"], &["d"]) {
+        Err(msg) => error(&msg),
+        Ok((flags, words, opts)) => {
+            println!("words:{}", words.join(","),);
+            println!("flags:{}", flags.join(","),);
+            println!(
+                "opts:{}",
+                opts.iter()
+                    .map(|(k, v)| format!("{}={}", k, v))
+                    .collect::<Vec<String>>()
+                    .join(",")
+            );
 
-    if let Some(home) = home_dir() {
-        let osoy_path = home.join(".osoy");
-        if osoy_path.is_dir() {
-            match args.get(1) {
-                Some(operator) => match operator.as_str() {
-                    "l" | "list" => list(
-                        osoy_path.join("packages").as_path(),
-                        osoy_path.join("bin").as_path(),
-                        &args[2..],
-                    ),
-                    "c" | "clone" => msg("to be implemented: clone <query>"),
-                    "r" | "remove" => msg("to be implemented: remove <query>"),
-                    "s" | "symlink" => msg("to be implemented: symlink [query]"),
-                    "u" | "update" => msg("to be implemented: update [query]"),
-                    "m" | "make" => msg("to be implemented: make [query]"),
-                    "dir" => msg("to be implemented: dir <query>"),
-                    "read" => msg("to be implemented: read <query>"),
-                    "license" => msg("to be implemented: license <query>"),
-                    _ => error(&format!("unknown operator '{}'", operator)),
-                },
-                None => print_usage(),
+            if let Some(home) = home_dir() {
+                let osoy_path = home.join(".osoy");
+                if osoy_path.is_dir() {
+                    match words.get(0) {
+                        Some(operator) => match operator.as_str() {
+                            "l" | "list" => list(
+                                osoy_path.join("packages").as_path(),
+                                osoy_path.join("bin").as_path(),
+                                &words[1..],
+                            ),
+                            "c" | "clone" => msg("to be implemented: clone <query>"),
+                            "r" | "remove" => msg("to be implemented: remove <query>"),
+                            "s" | "symlink" => msg("to be implemented: symlink [query]"),
+                            "u" | "update" => msg("to be implemented: update [query]"),
+                            "m" | "make" => msg("to be implemented: make [query]"),
+                            "dir" => msg("to be implemented: dir <query>"),
+                            "read" => msg("to be implemented: read <query>"),
+                            "license" => msg("to be implemented: license <query>"),
+                            _ => error(&format!("unknown operator '{}'", operator)),
+                        },
+                        None => print_usage(),
+                    }
+                } else {
+                    error("osoy directory not found")
+                }
+            } else {
+                error("home directory not found")
             }
-        } else {
-            error("osoy directory not found")
         }
-    } else {
-        error("home directory not found")
     }
 }
