8cfd452e9c47d362f222bf413c1fcad3fc5fd62e
rasmusmerzin <rasmusmerzin@gmail.com>
2020-08-29 17:17:14 +0300

    tweaked status header commits

--- src/operator.rs
+++ src/operator.rs
@@ -20,7 +20,7 @@ pub fn list(pkg_path: &Path, bin_path: &Path, query: &[String], color: bool) {
             if let Some(branch) = get_branch(&repo) {
                 if &branch != "master" {
                     if color {
-                        print!(" \u{1b}[1m\u{1b}[33m@{}\u{1b}[m", branch);
+                        print!(" \u{1b}[93m@{}\u{1b}[m", branch);
                     } else {
                         print!(" @{}", branch);
                     }
@@ -169,83 +169,89 @@ pub fn status(pkg_path: &Path, query: &[String], color: bool) {
                 if let Some(info) = get_status(&repo) {
                     let mut header = false;
 
-                    if let Some(branch) = info.branch {
-                        if &branch != "master" {
+                    let has_commits = info.commits_ahead > 0 || info.commits_behind > 0;
+
+                    if has_commits || info.files.len() > 0 {
+                        header = true;
+                    } else if let Some(branch) = &info.branch {
+                        if &*branch != "master" {
                             header = true;
-                            output.push_str(&rel_path.to_string_lossy());
+                        }
+                    } else if !header {
+                        if let Some(upstream) = &info.upstream {
+                            if upstream != "origin/master" {
+                                header = true;
+                            }
+                        } else {
+                            header = true;
+                        }
+                    }
+
+                    if header {
+                        output.push_str(&rel_path.to_string_lossy());
+
+                        if let Some(branch) = info.branch {
                             if color {
-                                output
-                                    .push_str(&format!(" \u{1b}[1m\u{1b}[33m@{}\u{1b}[m", branch));
+                                if &branch == "master" {
+                                    output.push_str(&format!(" \u{1b}[33m@{}\u{1b}[m", branch));
+                                } else {
+                                    output.push_str(&format!(" \u{1b}[93m@{}\u{1b}[m", branch));
+                                }
                             } else {
                                 output.push_str(&format!(" @{}", branch));
                             }
                         }
-                    }
 
-                    if header || info.commits_ahead > 0 || info.commits_behind > 0 {
-                        if !header {
-                            header = true;
-                            output.push_str(&rel_path.to_string_lossy());
-                        }
-                        output.push_str(&format!(
-                            " [{}:{}]",
-                            info.commits_ahead, info.commits_behind
-                        ));
-                    }
-
-                    if let Some(upstream) = info.upstream {
-                        if upstream != "origin/master" {
-                            if !header {
-                                header = true;
-                                output.push_str(&rel_path.to_string_lossy());
-                            }
-                            output.push_str(&format!(" ({})", upstream));
-                        }
-                    } else {
-                        if !header {
-                            header = true;
-                            output.push_str(&rel_path.to_string_lossy());
-                        }
-                        output.push_str(" (no remote)");
-                    }
-
-                    if header {
-                        output.push('\n');
-                    }
-
-                    if info.files.len() > 0 && !header {
-                        output.push_str(&format!("{}\n", rel_path.display()));
-                    }
-
-                    for file in info.files {
-                        if color {
+                        if color && has_commits {
                             output.push_str(&format!(
-                                "  {}{}: {}\n",
-                                match file.staged {
-                                    true => "\u{1b}[1m+",
-                                    false => "-",
-                                },
-                                match file.action {
-                                    GitAction::Delete => "\u{1b}[31mD\u{1b}[m",
-                                    GitAction::New => "\u{1b}[32mN\u{1b}[m",
-                                    GitAction::Modify => "\u{1b}[33mM\u{1b}[m",
-                                },
-                                file.location
+                                " \u{1b}[1m\u{1b}[34m[{}:{}]\u{1b}[m",
+                                info.commits_ahead, info.commits_behind
                             ));
                         } else {
                             output.push_str(&format!(
-                                "  {}{}: {}\n",
-                                match file.staged {
-                                    true => "+",
-                                    false => "-",
-                                },
-                                match file.action {
-                                    GitAction::Delete => "D",
-                                    GitAction::New => "N",
-                                    GitAction::Modify => "M",
-                                },
-                                file.location
+                                " [{}:{}]",
+                                info.commits_ahead, info.commits_behind
                             ));
+                        }
+
+                        if let Some(upstream) = info.upstream {
+                            output.push_str(&format!(" ({})", upstream));
+                        } else {
+                            output.push_str(" (no remote)");
+                        }
+
+                        output.push('\n');
+
+                        for file in info.files {
+                            if color {
+                                output.push_str(&format!(
+                                    "  {}{}: {}\n",
+                                    match file.staged {
+                                        true => "+",
+                                        false => "-",
+                                    },
+                                    match file.action {
+                                        GitAction::Delete => "\u{1b}[31mD\u{1b}[m",
+                                        GitAction::New => "\u{1b}[32mN\u{1b}[m",
+                                        GitAction::Modify => "\u{1b}[33mM\u{1b}[m",
+                                    },
+                                    file.location
+                                ));
+                            } else {
+                                output.push_str(&format!(
+                                    "  {}{}: {}\n",
+                                    match file.staged {
+                                        true => "+",
+                                        false => "-",
+                                    },
+                                    match file.action {
+                                        GitAction::Delete => "D",
+                                        GitAction::New => "N",
+                                        GitAction::Modify => "M",
+                                    },
+                                    file.location
+                                ));
+                            }
                         }
                     }
                 } else {
