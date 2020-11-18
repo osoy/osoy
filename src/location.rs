9624fa372a89e19db975552ab8aa0f30519bcecb
rasmusmerzin <rasmusmerzin@gmail.com>
2020-11-18 19:04:48 +0200

    completed location parsing

--- src/location.rs
+++ src/location.rs
@@ -2,13 +2,13 @@ use regex::Regex;
 use std::str::FromStr;
 use std::{error, fmt};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Protocol {
     Git,
     Other(String),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Location {
     protocol: Option<Protocol>,
     id: Vec<String>,
@@ -20,35 +20,49 @@ impl Location {
     }
 
     pub fn id(&self) -> String {
-        self.id.join("/")
+        match &self.protocol {
+            Some(_) => self.id.join("/"),
+            None => format!(
+                "{}{}{}",
+                match self.id.len() < 3 {
+                    true => "github.com/",
+                    false => "",
+                },
+                match self.id.len() == 1 {
+                    true => format!("{}/", self.id[0]),
+                    false => "".into(),
+                },
+                self.id.join("/"),
+            ),
+        }
+    }
+
+    pub fn url(&self) -> String {
+        match &self.protocol {
+            Some(Protocol::Other(p)) => format!("{}://{}", p, self.id.join("/")),
+            Some(Protocol::Git) => format!(
+                "{}{}",
+                self.id
+                    .get(0)
+                    .map(|domain| format!("git@{}:", domain))
+                    .unwrap_or("".to_string()),
+                self.id
+                    .get(1..)
+                    .map(|route| route.join("/"))
+                    .unwrap_or("".to_string())
+            ),
+            None => format!("https://{}", self.id(),),
+        }
     }
 }
 
 impl fmt::Display for Location {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match &self.protocol {
-                Some(Protocol::Other(p)) => format!("{}://{}", p, self.id.join("/")),
-                Some(Protocol::Git) => format!(
-                    "{}{}",
-                    self.id
-                        .get(0)
-                        .map(|domain| format!("git@{}:", domain))
-                        .unwrap_or("".to_string()),
-                    self.id
-                        .get(1..)
-                        .map(|route| route.join("/"))
-                        .unwrap_or("".to_string())
-                ),
-                _ => todo!(),
-            }
-        )
+        write!(f, "{}", self.id.join("/"))
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Copy)]
 pub struct ParseLocationError {}
 
 impl fmt::Display for ParseLocationError {
@@ -77,7 +91,7 @@ impl FromStr for Location {
             Ok(Self {
                 protocol: Some(Protocol::Git),
                 id: re_git
-                    .replace(s, "$1/")
+                    .replace(s, "$1$2/")
                     .split("/")
                     .map(|s| s.to_owned())
                     .collect(),
@@ -95,51 +109,54 @@ impl FromStr for Location {
 mod tests {
     use super::*;
 
-    #[test]
-    fn url() {
-        let check = |url, id| {
-            let location = Location::from_str(url).unwrap();
-            assert_eq!(location.to_string(), url);
-            assert_eq!(location.id(), id);
-        };
+    fn check(query: &str, url: &str, id: &str, display: &str) {
+        let location = Location::from_str(query).unwrap();
+        assert_eq!(location.url(), url);
+        assert_eq!(location.id(), id);
+        assert_eq!(location.to_string(), display);
+    }
 
+    #[test]
+    fn full() {
         check(
             "http://github.com/rasmusmerzin/hue",
+            "http://github.com/rasmusmerzin/hue",
+            "github.com/rasmusmerzin/hue",
             "github.com/rasmusmerzin/hue",
         );
         check(
             "git@gitlab.com:rasmusmerzin/archer",
+            "git@gitlab.com:rasmusmerzin/archer",
+            "gitlab.com/rasmusmerzin/archer",
             "gitlab.com/rasmusmerzin/archer",
         );
     }
 
     #[test]
-    fn query() {
-        let check = |query, url, id| {
-            let location = Location::from_str(query).unwrap();
-            assert_eq!(location.to_string(), url);
-            assert_eq!(location.id(), id);
-        };
-
+    fn partial() {
         check(
             "gitlab.com:rasmusmerzin/xhueloop",
             "git@gitlab.com:rasmusmerzin/xhueloop",
+            "gitlab.com/rasmusmerzin/xhueloop",
             "gitlab.com/rasmusmerzin/xhueloop",
         );
         check(
             "gitlab.com/rasmusmerzin/gol-java",
             "https://gitlab.com/rasmusmerzin/gol-java",
             "gitlab.com/rasmusmerzin/gol-java",
+            "gitlab.com/rasmusmerzin/gol-java",
         );
         check(
             "rasmusmerzin/recl",
             "https://github.com/rasmusmerzin/recl",
             "github.com/rasmusmerzin/recl",
+            "rasmusmerzin/recl",
         );
         check(
             "osoy",
             "https://github.com/osoy/osoy",
             "github.com/osoy/osoy",
+            "osoy",
         );
     }
 }
