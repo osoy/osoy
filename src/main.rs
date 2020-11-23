c73f860e6a523438cd338aa98a4d215ffb9b2888
rasmusmerzin <rasmusmerzin@gmail.com>
2020-11-23 19:12:57 +0200

    operate

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,16 @@
 mod config;
 mod location;
+mod operate;
 mod opt;
 mod util;
 
-use config::Config;
+pub use config::Config;
 pub use location::Location;
-use opt::*;
+use operate::operate;
+use opt::{Opt, StructOpt};
 
 fn main() {
-    let config = Config::from_env();
     let opt = Opt::from_args();
-
-    match opt.operator {
-        Operator::Completions { shell } => {
-            Opt::clap().gen_completions_to(env!("CARGO_PKG_NAME"), shell, &mut std::io::stdout());
-        }
-
-        Operator::List { targets, regex } => match util::iter_repos(&config.src) {
-            Ok(iter) => iter
-                .filter(|repo| {
-                    targets.len() == 0
-                        || targets.iter().any(|location| match regex {
-                            true => location.matches_re(&repo),
-                            false => location.matches(&repo),
-                        })
-                })
-                .for_each(|repo| {
-                    repo.strip_prefix(&config.src)
-                        .ok()
-                        .map(|rel| println!("{}", rel.display()));
-                }),
-            Err(err) => eprintln!("{}", err),
-        },
-
-        _ => todo!(),
-    }
+    let config = Config::from_env();
+    operate(opt, config);
 }
---
+++ src/operate.rs
@@ -0,0 +1,30 @@
+use crate::opt::{Operator, Opt, StructOpt};
+use crate::util;
+use crate::Config;
+
+pub fn operate(opt: Opt, config: Config) {
+    match opt.operator {
+        Operator::Completions { shell } => {
+            Opt::clap().gen_completions_to(env!("CARGO_PKG_NAME"), shell, &mut std::io::stdout());
+        }
+
+        Operator::List { targets, regex } => match util::iter_repos(&config.src) {
+            Ok(iter) => iter
+                .filter(|repo| {
+                    targets.len() == 0
+                        || targets.iter().any(|location| match regex {
+                            true => location.matches_re(&repo),
+                            false => location.matches(&repo),
+                        })
+                })
+                .for_each(|repo| {
+                    repo.strip_prefix(&config.src)
+                        .ok()
+                        .map(|rel| println!("{}", rel.display()));
+                }),
+            Err(err) => eprintln!("{}", err),
+        },
+
+        _ => todo!(),
+    }
+}
