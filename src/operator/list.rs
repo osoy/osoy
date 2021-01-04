8575a55f0a779be29a94e4cd3818848c9460ddfe
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2021-01-04 09:48:57 +0200

    list executables

--- src/operator/list.rs
+++ src/operator/list.rs
@@ -1,10 +1,13 @@
-use crate::{repo, Config, Exec, Location, StructOpt};
+use crate::{link, repo, Config, Exec, Location, StructOpt};
+use std::path::PathBuf;
 
 #[derive(StructOpt, Debug)]
 #[structopt(alias = "ls", about = "List repositories")]
 pub struct Opt {
     #[structopt(short, long, help = "Use regular expressions")]
     regex: bool,
+    #[structopt(short, long, help = "List executables", parse(from_occurrences))]
+    executables: u8,
     #[structopt(help = Location::about())]
     targets: Vec<Location>,
 }
@@ -12,11 +15,62 @@ pub struct Opt {
 impl Exec for Opt {
     fn exec(self, config: Config) {
         match repo::iterate_matching_exists(&config.src, self.targets, self.regex) {
-            Ok(iter) => iter.for_each(|path| {
-                path.strip_prefix(&config.src)
-                    .ok()
-                    .map(|rel| println!("{}", rel.display()));
-            }),
+            Ok(iter) => {
+                let repos = iter.collect::<Vec<PathBuf>>();
+                let exe_flag = self.executables;
+
+                let symlinks = match exe_flag {
+                    0 => None,
+                    _ => Some(
+                        link::iterate(&config.bin, repos.clone())
+                            .map(|iter| iter.collect::<Vec<(PathBuf, PathBuf)>>())
+                            .unwrap_or(vec![]),
+                    ),
+                };
+
+                for path in repos {
+                    let exes = match exe_flag {
+                        0 => None,
+                        _ => link::executables(&path).ok().map(|iter| {
+                            iter.filter_map(|exe| {
+                                let symbolics = symlinks
+                                    .clone()
+                                    .unwrap()
+                                    .iter()
+                                    .filter_map(|(sym, dest)| match dest == &exe {
+                                        true => {
+                                            Some(sym.strip_prefix(&config.bin).unwrap().display())
+                                        }
+                                        false => None,
+                                    })
+                                    .fold(String::new(), |acc, sym| match acc.is_empty() {
+                                        true => format!(" <- {}", sym),
+                                        false => format!("{}, {}", acc, sym),
+                                    });
+
+                                match exe_flag == 1 || !symbolics.is_empty() {
+                                    true => Some(format!(
+                                        "{}{}",
+                                        exe.strip_prefix(&path).unwrap().display(),
+                                        symbolics,
+                                    )),
+                                    false => None,
+                                }
+                            })
+                            .collect::<Vec<String>>()
+                        }),
+                    };
+
+                    println!(
+                        "{}{}",
+                        path.strip_prefix(&config.src).unwrap().display(),
+                        exes.map(|lines| lines
+                            .iter()
+                            .fold(String::new(), |acc, line| format!("{}\n  {}", acc, line)))
+                            .unwrap_or(String::new())
+                    );
+                }
+            }
             Err(err) => info!("{}", err),
         }
     }
