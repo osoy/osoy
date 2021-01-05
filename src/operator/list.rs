2a5528044968caabb4d1d253e000a599a97b1bba
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2021-01-05 10:37:45 +0200

    list git files

--- src/operator/list.rs
+++ src/operator/list.rs
@@ -1,21 +1,62 @@
 use crate::{link, repo, Config, Exec, Location, StructOpt};
+use git2::{Repository, Status};
+use structopt::clap::ArgGroup;
 
 #[derive(StructOpt, Debug)]
-#[structopt(alias = "ls", about = "List repositories")]
+#[structopt(alias = "ls", about = "List repositories", group = ArgGroup::with_name("sublist"))]
 pub struct Opt {
     #[structopt(short, long, help = "Use regular expressions")]
     regex: bool,
-    #[structopt(short, long, help = "List executables", parse(from_occurrences))]
-    executables: u8,
+    #[structopt(
+        short,
+        long,
+        group = "sublist",
+        help = "List executables",
+        parse(from_occurrences)
+    )]
+    exe: u8,
+    #[structopt(
+        short,
+        long,
+        group = "sublist",
+        help = "Show git statuses",
+        parse(from_occurrences)
+    )]
+    git: u8,
+    #[structopt(short, long, help = "Show only entries with details")]
+    only_details: bool,
     #[structopt(help = Location::about())]
     targets: Vec<Location>,
+}
+
+fn status_wt_char(st: &Status) -> Option<char> {
+    match st {
+        s if s.contains(Status::WT_NEW) => Some('N'),
+        s if s.contains(Status::WT_MODIFIED) => Some('M'),
+        s if s.contains(Status::WT_DELETED) => Some('D'),
+        s if s.contains(Status::WT_RENAMED) => Some('R'),
+        s if s.contains(Status::WT_TYPECHANGE) => Some('T'),
+        _ => None,
+    }
+}
+
+fn status_index_char(st: &Status) -> Option<char> {
+    match st {
+        s if s.contains(Status::INDEX_NEW) => Some('N'),
+        s if s.contains(Status::INDEX_MODIFIED) => Some('M'),
+        s if s.contains(Status::INDEX_DELETED) => Some('D'),
+        s if s.contains(Status::INDEX_RENAMED) => Some('R'),
+        s if s.contains(Status::INDEX_TYPECHANGE) => Some('T'),
+        _ => None,
+    }
 }
 
 impl Exec for Opt {
     fn exec(self, config: Config) {
         match repo::iterate_matching_exists(&config.src, self.targets, self.regex) {
             Ok(iter) => {
-                let exe_flag = self.executables;
+                let exe_flag = self.exe;
+                let git_flag = self.git;
 
                 let symlinks = match exe_flag {
                     0 => None,
@@ -27,7 +68,7 @@ impl Exec for Opt {
                 };
 
                 for path in iter {
-                    let exes = match exe_flag {
+                    let exe_lines = match exe_flag {
                         0 => None,
                         _ => link::executables(&path).ok().map(|iter| {
                             iter.filter_map(|exe| {
@@ -55,18 +96,55 @@ impl Exec for Opt {
                                     false => None,
                                 }
                             })
-                            .collect::<Vec<String>>()
+                            .fold(String::new(), |acc, line| format!("{}\n  {}", acc, line))
                         }),
-                    };
+                    }
+                    .unwrap_or("".into());
 
-                    println!(
-                        "{}{}",
-                        path.strip_prefix(&config.src).unwrap().display(),
-                        exes.map(|lines| lines
-                            .iter()
-                            .fold(String::new(), |acc, line| format!("{}\n  {}", acc, line)))
-                            .unwrap_or(String::new())
-                    );
+                    let git_lines = match git_flag {
+                        0 => None,
+                        _ => Repository::open(&path)
+                            .ok()
+                            .map(|repo| {
+                                repo.statuses(None).ok().map(|statuses| {
+                                    statuses
+                                        .iter()
+                                        .map(|entry| {
+                                            let st = entry.status();
+                                            let index_ch = status_index_char(&st);
+                                            let wt_ch = match git_flag {
+                                                1 => status_wt_char(&st),
+                                                _ => None,
+                                            };
+                                            let fname = String::from_utf8_lossy(entry.path_bytes());
+                                            vec![
+                                                index_ch
+                                                    .map(|ch| format!("{}* {}", ch, fname))
+                                                    .unwrap_or("".into()),
+                                                wt_ch
+                                                    .map(|ch| format!("{}  {}", ch, fname))
+                                                    .unwrap_or("".into()),
+                                            ]
+                                        })
+                                        .flatten()
+                                        .filter(|word| !word.is_empty())
+                                        .fold(String::new(), |acc, line| {
+                                            format!("{}\n  {}", acc, line)
+                                        })
+                                })
+                            })
+                            .flatten(),
+                    }
+                    .unwrap_or("".into());
+
+                    if !self.only_details || !exe_lines.is_empty() || !git_lines.is_empty() {
+                        println!(
+                            "{}{}{}",
+                            path.strip_prefix(&config.src).unwrap().display(),
+                            exe_lines,
+                            git_lines
+                        );
+                    }
                 }
             }
             Err(err) => info!("{}", err),
