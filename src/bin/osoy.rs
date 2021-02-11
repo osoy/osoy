9d76bd2980ab6de978e84b72f1752254de2cb324
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2021-02-11 22:01:25 +0200

    added support for extensions

--- src/bin/osoy.rs
+++ src/bin/osoy.rs
@@ -1,12 +1,24 @@
 use osoy::{Config, Exec, Operator};
+use std::{env, process};
 use structopt::StructOpt;
 
 fn main() {
     match Operator::from_args_safe() {
-        Ok(operator) => {
-            let config = Config::from_env();
-            std::process::exit(operator.exec(config))
+        Ok(operator) => process::exit(operator.exec(Config::from_env())),
+        Err(err) => {
+            let args = env::args().skip(1).collect::<Vec<String>>();
+            args.get(0)
+                .map(|exe| {
+                    process::Command::new(&format!("{}-{}", env!("CARGO_PKG_NAME"), exe))
+                        .args(&args[1..])
+                        .status()
+                        .ok()
+                })
+                .flatten()
+                .map(|status| status.code())
+                .flatten()
+                .map(process::exit);
+            err.exit()
         }
-        Err(err) => err.exit(),
     }
 }
