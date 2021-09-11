205880865123b79f83039b2e4bddc74bebb2aef9
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2021-09-11 18:26:10 +0300

    refactored execute operator

--- src/operator/execute.rs
+++ src/operator/execute.rs
@@ -1,6 +1,7 @@
 use crate::{repo, Config, Exec, Location};
 use std::env::set_current_dir;
-use std::process;
+use std::io::{stdout, Write};
+use std::process::{Command, Stdio};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -8,8 +9,8 @@ use structopt::StructOpt;
 pub struct Opt {
     #[structopt(short, long, help = "Use regular expressions")]
     pub regex: bool,
-    #[structopt(short, long, help = "Show detailed output")]
-    pub verbose: bool,
+    #[structopt(short, long, help = "Less output")]
+    pub quiet: bool,
     #[structopt(help = Location::about())]
     pub target: Location,
     #[structopt(help = "Command to execute in the repository")]
@@ -22,44 +23,39 @@ impl Exec for Opt {
     fn exec(self, config: Config) -> i32 {
         let mut errors = 0;
 
+        let quiet = self.quiet;
+        let io_dest = || quiet.then(|| Stdio::null()).unwrap_or(Stdio::inherit());
+
         match repo::iterate_matching_exists(&config.src, vec![self.target], self.regex) {
             Ok(iter) => {
                 for path in iter {
+                    if quiet {
+                        print!("{}..", path.strip_prefix(&config.src).unwrap().display());
+                        stdout().flush().ok();
+                    } else {
+                        println!("{}", path.strip_prefix(&config.src).unwrap().display());
+                    }
                     match set_current_dir(&path) {
-                        Ok(_) => match process::Command::new(&self.command)
+                        Ok(_) => match Command::new(&self.command)
                             .args(&self.arguments)
-                            .stdin(match self.verbose {
-                                true => process::Stdio::inherit(),
-                                false => process::Stdio::null(),
-                            })
-                            .stderr(match self.verbose {
-                                true => process::Stdio::inherit(),
-                                false => process::Stdio::null(),
-                            })
-                            .stdout(match self.verbose {
-                                true => process::Stdio::inherit(),
-                                false => process::Stdio::null(),
-                            })
+                            .stdin(io_dest())
+                            .stderr(io_dest())
+                            .stdout(io_dest())
                             .env("PWD", path.display().to_string())
                             .status()
                         {
-                            Ok(status) => println!(
-                                "{}::{}",
-                                path.strip_prefix(&config.src).unwrap().display(),
-                                status
-                                    .code()
-                                    .map(|c| match c {
-                                        0 => "OK".into(),
-                                        _ => {
-                                            errors += 1;
-                                            format!("E{}", c)
-                                        }
-                                    })
-                                    .unwrap_or_else(|| {
-                                        errors += 1;
-                                        "NONE".into()
-                                    })
-                            ),
+                            Ok(status) => {
+                                let code = status.code();
+                                if code.as_ref().map_or(true, |c| *c != 0) {
+                                    errors += 1;
+                                }
+                                println!(
+                                    "{}",
+                                    status.code().map_or("NONE".into(), |c| (c != 0)
+                                        .then(|| format!("E{}", c))
+                                        .unwrap_or("OK".into()))
+                                )
+                            }
                             Err(err) => {
                                 errors += 1;
                                 info!("failed to execute '{}': {}", self.command, err)
